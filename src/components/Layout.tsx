0ed47750c3cfd5909783bcc36293886200bd4e1f
Duprious <superluigi455@gmail.com>
2022-12-31 02:49:46 +0100

    Please OG just work

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,25 +10,6 @@ interface LayoutProps {
 const Layout: NextPage<LayoutProps> = ({children}) => {
   return (
     <>
-      <Head>
-        <title>Krunker Competitive Hub</title>
-        <meta name="description" content="All info on Competitive Krunker here" />
-        <meta property="og:url" content="https://kch.vercel.app/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Krunker Competitive Hub" />
-        <meta property="og:description" content="All info on Competitive Krunker here" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image" content="https://i.ibb.co/KLzJbYg/JTz-Oke7a-2x.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="kch.vercel.app" />
-        <meta property="twitter:url" content="https://kch.vercel.app/" />
-        <meta name="twitter:title" content="Krunker Competitive Hub" />
-        <meta name="twitter:description" content="All info on Competitive Krunker here" />
-        <meta name="twitter:image" content="https://i.ibb.co/KLzJbYg/JTz-Oke7a-2x.png" />
-
-        
-      </Head>
       <div className='min-h-screen'>
         <Navbar />
         {children}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import NextNProgress from 'nextjs-progressbar';
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -14,6 +15,23 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <NextNProgress options={{ showSpinner: false, easing: 'ease'}} />
+        <Head>
+          <title>Krunker Competitive Hub</title>
+          <meta name="description" content="All info on Competitive Krunker here" />
+          <meta property="og:url" content="https://kch.vercel.app/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Krunker Competitive Hub" />
+          <meta property="og:description" content="All info on Competitive Krunker here" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:image" content="https://i.ibb.co/KLzJbYg/JTz-Oke7a-2x.png" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="kch.vercel.app" />
+          <meta property="twitter:url" content="https://kch.vercel.app/" />
+          <meta name="twitter:title" content="Krunker Competitive Hub" />
+          <meta name="twitter:description" content="All info on Competitive Krunker here" />
+          <meta name="twitter:image" content="https://i.ibb.co/KLzJbYg/JTz-Oke7a-2x.png" />
+        </Head>
         <div className="dark dark:bg-gray-900 dark:text-white">
           <Component {...pageProps} />
         </div>
