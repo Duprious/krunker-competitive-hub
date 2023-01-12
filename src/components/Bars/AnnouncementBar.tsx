127471dc3706b4903bc3b5d77e11d0dc382e9aa3
Duprious <superluigi455@gmail.com>
2023-01-12 09:42:11 +0100

    Changed AnnouncementBar to KPC Tourney Reminder

--- src/components/Bars/AnnouncementBar.tsx
+++ src/components/Bars/AnnouncementBar.tsx
@@ -10,10 +10,10 @@ const AnnouncementBar = () => {
     <div className='bg-gray-900 text-white flex justify-between items-center py-2 text-xs '>
       <Marquee speed={30} gradient={false}>
         <div className='flex items-center mr-8 '>
-          <span className='mr-32'>Congrats to Gahou for winning the NA $250 3v3 Tournament</span>
-          <span className='mr-32'>Congrats to Gahou for winning the NA $250 3v3 Tournament</span>
-          <span className='mr-32'>Congrats to Gahou for winning the NA $250 3v3 Tournament</span>
-          <span className='mr-32'>Congrats to Gahou for winning the NA $250 3v3 Tournament</span>
+          <span className='mr-32'>KPC EU 2v2 Tournament Signups Closing Soon</span>
+          <span className='mr-32'>KPC EU 2v2 Tournament Signups Closing Soon</span>
+          <span className='mr-32'>KPC EU 2v2 Tournament Signups Closing Soon</span>
+          <span className='mr-32'>KPC EU 2v2 Tournament Signups Closing Soon</span>
         </div>
       </Marquee>
       <button onClick={setAnnouncementBarClosed}>
