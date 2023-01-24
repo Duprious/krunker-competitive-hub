6ef6ddc76fd6cb2506914093f5a19abf44e8fae1
Duprious <superluigi455@gmail.com>
2023-01-24 21:55:53 +0100

    Fixed footer and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Krunker Tournament Hub
+
+# Krunker Competitive Hub
 
 Website that shows all info surrounding the Krunker Competitive Scene made by [Duprious](https://twitter.com/Duprious)
+
+
+## Authors
+
+- [@Duprious](https://github.com/Duprious)
+
+
+## Support
+
+For support, email info@kchub.net.
--- src/components/Bars/Footer/Footer.tsx
+++ src/components/Bars/Footer/Footer.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">2023 <a href="https://kchub.net" className="hover:underline">KCH</a>. Krunker Competitive Hub
+    <footer className="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-800">
+      <span className="text-sm  sm:text-center text-gray-400">2023 <a href="https://kchub.net" className="hover:underline">KCH</a>. Krunker Competitive Hub
       </span>
-      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-400 sm:mt-0">
         <li>
           <Link href="/privacy" className='mr-4 hover:underline md:mr-6'>
             Privacy Policy
