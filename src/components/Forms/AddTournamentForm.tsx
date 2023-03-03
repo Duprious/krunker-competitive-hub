b8bf4461d67d9508f6b3028d1080166e72089f9a
Duprious <duprious@gmail.com>
2023-03-03 23:25:02 +0100

    fixed colors on add tournament page

--- src/components/Forms/AddTournamentForm.tsx
+++ src/components/Forms/AddTournamentForm.tsx
@@ -70,7 +70,7 @@ const AddTournamentForm = () => {
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="relative z-0 mb-6 w-full group">
-            <input type="text" id="name" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("name")} />
+            <input type="text" id="name" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("name")} />
             <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Tournament Name</label>
             {errors.name && (
             <p className="text-xs italic text-red-500 mt-2">
@@ -79,7 +79,7 @@ const AddTournamentForm = () => {
           )}
         </div>
         <div className="relative z-0 mb-6 w-full group">
-            <input type="text" id="description" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("description")} />
+            <input type="text" id="description" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("description")} />
             <label htmlFor="description" className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Description</label>
             {errors.description && (
             <p className="text-xs italic text-red-500 mt-2">
@@ -89,7 +89,7 @@ const AddTournamentForm = () => {
         </div>
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 mb-6 w-full group">
-              <input type="text" id="hostName" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("hostName")} />
+              <input type="text" id="hostName" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("hostName")} />
               <label htmlFor="hostName" className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Host (Organizer Name)</label>
               {errors.hostName && (
                 <p className="text-xs italic text-red-500 mt-2">
@@ -98,7 +98,7 @@ const AddTournamentForm = () => {
               )}
           </div>
           <div className="relative z-0 mb-6 w-full group">
-              <input type="number" id="maxTeams" min={0} max={64} className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("maxTeams", { setValueAs: (v) => v === "" ? undefined : parseInt(v, 10) })} />
+              <input type="number" id="maxTeams" min={0} max={64} className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("maxTeams", { setValueAs: (v) => v === "" ? undefined : parseInt(v, 10) })} />
               <label htmlFor="maxTeams" className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Max Teams</label>
               {errors.maxTeams && (
                 <p className="text-xs italic text-red-500 mt-2">
@@ -109,7 +109,7 @@ const AddTournamentForm = () => {
         </div>
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 mb-6 w-full group">
-            <input type="datetime-local" id="startDate" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("dates.startDate")} />
+            <input type="datetime-local" id="startDate" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("dates.startDate")} />
             <label htmlFor="startDate" className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Start Date</label>
             {errors.dates?.startDate && (
             <p className="text-xs italic text-red-500 mt-2">
@@ -118,7 +118,7 @@ const AddTournamentForm = () => {
           )}
           </div>
           <div className="relative z-0 mb-6 w-full group">
-            <input type="datetime-local" id="endDate" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none text-white border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("dates.endDate")} />
+            <input type="datetime-local" id="endDate" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("dates.endDate")} />
             <label htmlFor="endDate" className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">End Date</label>
             {errors.dates?.endDate && (
             <p className="text-xs italic text-red-500 mt-2">
@@ -133,7 +133,7 @@ const AddTournamentForm = () => {
           </div>
           <div className="relative z-0 mb-6 w-full group">
             <label htmlFor="type" className="block mb-2 text-sm font-medium text-gray-400">Select Tournament Type</label>
-            <select id="type" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" {...register("type")} >
+            <select id="type" className="border text-sm rounded-lg block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 placeholder-gray-400 dark:text-white text-black focus:ring-blue-500 focus:border-blue-500" {...register("type")} >
               <option>4v4</option>
               <option>3v3</option>
               <option>2v2</option>
@@ -146,7 +146,7 @@ const AddTournamentForm = () => {
           </div>
           <div className="relative z-0 mb-6 w-full group">
           <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-400">Select Tournament Server</label>
-            <select id="countries" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" {...register("region")} >
+            <select id="countries" className="border text-sm rounded-lg block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 placeholder-gray-400 dark:text-white text-black focus:ring-blue-500 focus:border-blue-500" {...register("region")} >
               <option>EU | FRA</option>
               <option>NA | NY</option>
               <option>NA | MIA</option>
@@ -170,7 +170,7 @@ const AddTournamentForm = () => {
           </div>
           <div className="relative z-0 mb-6 w-full group">
           <label htmlFor="organization" className="block mb-2 text-sm font-medium text-gray-400">Select Organization</label>
-            <select id="organization" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" {...register("organization")} >
+            <select id="organization" className="border text-sm rounded-lg block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 placeholder-gray-400 dark:text-white text-black focus:ring-blue-500 focus:border-blue-500" {...register("organization")} >
               <option value="clcv1ikrt00007ke4zq0wiy6l">KrunkerEsports</option>
               <option value="clcnvr1df00007klsxzg9xrb0">KPC</option>
               <option value="clcnvrbfi00027klsbqbey7i8">NACK</option>
@@ -184,7 +184,7 @@ const AddTournamentForm = () => {
             )}
           </div>
         </div>
-        <button type="submit" className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Submit</button>
+        <button type="submit" className="dark:text-white text-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Submit</button>
       </form>
       <Toaster />
     </>
