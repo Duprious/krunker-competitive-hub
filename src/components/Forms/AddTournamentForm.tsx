72298b7891a34ecf140fc5e3a098fa7c78240092
Duprious <superluigi455@gmail.com>
2023-03-11 01:13:50 +0100

    updated datetime picker colors and nicer scrollbar

--- src/components/Forms/AddTournamentForm.tsx
+++ src/components/Forms/AddTournamentForm.tsx
@@ -109,7 +109,7 @@ const AddTournamentForm = () => {
         </div>
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 mb-6 w-full group">
-            <input type="datetime-local" id="startDate" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("dates.startDate")} />
+            <input type="datetime-local" id="startDate" className="dark:calendar-white block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("dates.startDate")} />
             <label htmlFor="startDate" className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Start Date</label>
             {errors.dates?.startDate && (
             <p className="text-xs italic text-red-500 mt-2">
@@ -118,7 +118,7 @@ const AddTournamentForm = () => {
           )}
           </div>
           <div className="relative z-0 mb-6 w-full group">
-            <input type="datetime-local" id="endDate" className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("dates.endDate")} />
+            <input type="datetime-local" id="endDate" className="dark:calendar-white block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 appearance-none dark:text-white text-black border-gray-600 focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " {...register("dates.endDate")} />
             <label htmlFor="endDate" className="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">End Date</label>
             {errors.dates?.endDate && (
             <p className="text-xs italic text-red-500 mt-2">
